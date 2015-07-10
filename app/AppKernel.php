9a30bf0f6651693d01d6ca2c2ce1e712123d2fb1
Djuro Mandinic <djuro.mandinic@gmail.com>
2015-07-11 01:18:17 +0200

    Added MongoDB, FosRest,JMSSerializer bundles.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new Cg\KintBundle\CgKintBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
