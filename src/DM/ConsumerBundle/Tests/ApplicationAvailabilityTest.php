d577507dd4fb4c4839f6cca0f6e90470a4544084
djuro <djuro.mandinic@gmail.com>
2015-08-24 22:24:45 +0200

    Added Phpunit and a simple functional test.

---
+++ src/DM/ConsumerBundle/Tests/ApplicationAvailabilityTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace DMConsumerBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Basic, smoke testing. Detect if some page/route is unavailable.
+ * 
+ */
+class ApplicationAvailabilityTest extends WebTestCase
+{
+    public function testPageIsSuccessful()
+    {
+        $client = self::createClient();
+        
+        $client->request('POST', '/new-message');
+        $this->assertTrue($client->getResponse()->isSuccessful(),"New message' endpoint is not available.");
+        
+        $client->request('GET', '/login');
+        $this->assertTrue($client->getResponse()->isSuccessful(),"Login page is not available.");
+    }
+    
+}
